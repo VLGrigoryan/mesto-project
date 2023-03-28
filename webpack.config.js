d8b231800af99d787343daaf20507fdbcba06639
VLGrigoryan <v.grigoryan@live.com>
2023-03-28 15:42:06 +0200

    Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -21,25 +21,25 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: ['/node_modules/']
+    },
+    {
+      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1
+        }
       },
-      {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      },
+        'postcss-loader'
+      ]
+    },
     ]
   },
   plugins: [
@@ -47,7 +47,7 @@ module.exports = {
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({ filename: 'index.css' }),
 
   ]
 }
